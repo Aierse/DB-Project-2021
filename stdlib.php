2249478a9f7639590f241aa8c7e3fca4e21fa126
Aierse <68111814+Aierse@users.noreply.github.com>
2021-05-24 21:16:23 +0900

    search_member 구현
    테스트 필요

--- stdlib.php
+++ stdlib.php
@@ -1,5 +1,5 @@
 <?php
-    function get_connect($user_id, $user_pw) {
+    function get_connect($user_id = "dbuser174414", $user_pw = "ce1234") {
       $dbsid = "( DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP) (HOST = localhost) (PORT = 1521) ) ) 
                           (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = orcl) ) ) ";
       
@@ -16,8 +16,19 @@
     }
 
     function search_member($connect, $name) {
-        $sql = "SELECT * FROM Customer WHERE name = $name";
+        $sql = "SELECT TOP 1 user_id FROM Customer WHERE name = $name";
         
-        /*미완성*/
+        $stid = oci_parse($conn,$sql);
+        if (oci_execute($stid)) {
+            $row = oci_fetch_array($result, OCI_ASSOC);
+            
+            oci_free_statement($stid);
+            oci_close($connect);
+            
+            if (count($row))
+                return $row;
+        }
+        
+        return NULL;
     }
 ?>
