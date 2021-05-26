48642d34d4baa83a84d4794bb646c045a2506e7f
Aierse <68111814+Aierse@users.noreply.github.com>
2021-05-26 20:45:04 +0900

    Seat.php 생성

---
+++ seat.php
@@ -0,0 +1,39 @@
+<style>
+    .seat {
+        margin : auto;
+        margin-top : 50px;
+        border : 1px solid gray;
+        padding : 5px;
+        border-spacing: 5px;
+    }
+
+    .screen {
+        font-size : 80px;
+        color : gray;
+        letter-spacing : 16px;
+    }
+
+    .seat td {
+        border : 1px solid black;
+        padding : 20px;
+        margin : 3px;
+    }
+</style>
+<table class = "seat">
+    <caption class = "screen">스크린</caption>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1-1</td>
+            <td>1-2</td>
+            <td>1-3</td>
+            <td>1-4</td>
+            <td>1-5</td>
+            <td>1-6</td>
+            <td>1-7</td>
+            <td>1-8</td>
+            <td>1-9</td>
+            <td>1-10</td>
+        </tr>
+    </tbody>
+</table>
