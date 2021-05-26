2f73f429d7e6acd342244ea8115064d47f89f7ce
Aierse <68111814+Aierse@users.noreply.github.com>
2021-05-26 20:01:10 +0900

    ID/PW 찾기 및 수정 쿼리

--- stdlib.php
+++ stdlib.php
@@ -7,6 +7,33 @@
 
         return $connect;
     }
+    // 쿼리 실행과 해제를 담당
+    function query_with_disconnect($connnect, $stid, $sql) {
+        if (explode(' ',$sql)[0] == "SELECT") {
+            if (oci_execute($stid)) {
+            $row = oci_fetch_array($stid, OCI_ASSOC);
+            
+            oci_free_statement($stid);
+            oci_close($connect);
+            
+            if (count($row)) // 결과가 있다면 결과를 반환
+                return $row;
+            }
+
+            return null;
+        }
+        
+        else if (explode(' ',$sql)[0] == "UPDATE") {
+            if (oci_execute($stid)) {
+                oci_free_statement($stid);
+                oci_close($connect);
+
+                return true;
+            }
+            
+            return false;
+        }
+    }
 
     function check_input($data) {
         $data = trim($data);
@@ -14,26 +41,41 @@
         $data = htmlspecialchars($data);
         return $data;
     }
-
+    // 아이디로 멤버 아이디 검색
     function search_member($user_id, $connect = null) {
         if (!isset($connect))
             $connect = get_connect();
         
         $sql = "SELECT user_id FROM Customer WHERE user_id = :user_id";
-        $stid = oci_parse($connect,$sql);
+        $stid = oci_parse($connect, $sql);
         
         oci_bind_by_name($stid, ":user_id", $user_id);
 
-        if (oci_execute($stid)) {
-            $row = oci_fetch_array($stid, OCI_ASSOC);
-            
-            oci_free_statement($stid);
-            oci_close($connect);
-            
-            if (count($row))
-                return $row;
-        }
+        return query_with_disconnect($connect, $stid, $sql);
+    }
+    // 휴대폰 번호로 멤버 ID 찾기
+    function search_member_id($user_phone_number, $connect = null) {
+        if (!isset($connect))
+            $connect = get_connect();
         
-        return NULL;
+        $sql = "SELECT user_id FROM Customer WHERE user_phone_number = :user_phone_number";
+        $stid = oci_parse($connect, $sql);
+
+        oci_bind_by_name($stid, ":user_phone_number", $user_phone_number);
+
+        return query_with_disconnect($connect, $stid, $sql);
+    }
+
+    function update_member_pw($user_id, $user_phone_number, $connect = null) {
+        if (!isset($connect))
+            $connect = get_connect();
+        
+        $sql = "UPDATE Customer SET user_pw WHERE user_id = :user_id AND user_phone_number = :user_phone_number";
+        $stid = oci_parse($connect, $sql);
+
+        oci_bind_by_name($stid, ":user_id", $user_id);
+        oci_bind_by_name($stid, ":user_phone_number", $user_phone_number);
+
+        return query_with_disconnect($connect, $stid, $sql);
     }
 ?>
