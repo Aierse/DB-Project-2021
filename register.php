4d078605ac7911ff9dc1ded77ca6e6dbd5cb844c
ChoiXD <84668477+ChoiXD@users.noreply.github.com>
2021-05-27 19:38:15 +0900

    register.php 레이아웃 통일

--- register.php
+++ register.php
@@ -1,165 +1,134 @@
 <!DOCTYPE html>
 <html>
-<head>
+	<head>
+		<meta charset="utf-8"/>
+			<title>회원가입 페이지</title>
+		<style>
+			h, td, tr, input,button, textarea, select, FORM{
+				font-family:고딕;
+				font-size:1em;
+				border-radius:5px;
+			}
 
-  <meta charset="utf-8"/>
-  <title>회원가입 페이지</title>
+			table{
+				border-spacing:15px;
+				text-align:center;
+				margin-left:auto; 
+				margin-right:auto;
+				width:500px;
+			}
+		</style>
+		<script>
+			function checkNumber(event){
+				if(event.key === '.' || event.key === '-' || event.key >= 0 && event.key <= 9){
+					return true;
+				}
+				return false;
+			}
+			function checkId() {
+				window.open("checkId.php?id=" + document.register.id.value,"IDcheck","left=50, top=50, width=100, height=20, scrollbars=no, resizeable=no");
+			}
 
-  <style>
- h, td, tr, input,button, textarea, select, FORM{
-   font-family:고딕;
-  font-size:1em;
-  border-radius:5px;
-  
- }
- 
- table{
-  border-spacing:15px;
-  text-align:center;
-	margin-left:auto; 
-	margin-right:auto;
-	width:500px;
- }
+			function checkValue(){
+				if(!document.register.id.value){
+					alert("아이디를 입력하세요.");
+					return false;
+				}
 
-  </style>
-  <script>
-    function checkNumber(event) {
-      if(event.key === '.' 
-         || event.key === '-'
-         || event.key >= 0 && event.key <= 9) {
-        return true;
-      }
+				if(!document.register.pw.value){
+					alert("비밀번호를 입력하세요.");
+					return false;
+				}
 
-      return false;
-    }
-    
-    function checkId() {
-      window.open("checkId.php?id=" + document.register.id.value,"IDcheck","left=50, top=50, width=100, height=20, scrollbars=no, resizeable=no");
-    }
-    
-    function checkValue()
-        {
-            if(!document.register.id.value){
-                alert("아이디를 입력하세요.");
-                return false;
-            }
-            
-            if(!document.register.pw.value){
-                alert("비밀번호를 입력하세요.");
-                return false;
-            }
-            
-            // 비밀번호와 비밀번호 확인에 입력된 값이 동일한지 확인
-            if(document.register.pw.value != document.register.pwCheck.value){
-                alert("비밀번호를 동일하게 입력하세요.");
-                return false;
-            }
-          
-            if(!document.register.name.value){
-                alert("이름을 입력하세요.");
-                return false;
-            }
-            
-            if(!document.register.FirstNum.value || !document.register.middleNum.value || !document.register.lastNum.value){
-                alert("휴대폰번호를 입력하세요.");
-                return false;
-            }
-        }
-  </script>
-</head>
-<body>
- <h1>회원가입</h1>
- <table boder = "" bgcolor = "#cdfdee" cellspacing = "1" >
- 
- <FORM name="register" method="post" action="./insertMember.php" onsubmit="return checkValue()">
+				// 비밀번호와 비밀번호 확인에 입력된 값이 동일한지 확인
+				if(document.register.pw.value != document.register.pwCheck.value){
+					alert("비밀번호를 동일하게 입력하세요.");
+					return false;
+				}
 
-<tr>
+				if(!document.register.name.value){
+					alert("이름을 입력하세요.");
+					return false;
+				}
 
-<td text-align="center">아이디 </td>
-
-   <td>
-   <input type = "text" name="id" maxlength = "10"/>
-   </td>
-   <td>
-   <input type = "button" value = "중복확인" onClick = "checkId()"/>
-   </td>
-
-  </tr>
-  
-  <tr>
-   <td> 비밀번호 </td>
-   <td> <input type = "password" name="pw" maxlength = "10"/> </td>
-  </tr>
-
-  <tr>
-   <td> 비밀번호 확인 </td>
-   <td> <input type = "password" name="pwCheck" maxlength = "10"/> </td>
-  </tr>
-
-  <tr>
-   <td> 성명 </td>
-   <td> <input type = "text" name="name" maxlength = "10"/> </td>
-  </tr>
-  
-  <tr>
-   <td> 생년월일 </td>
-   <td>
-    <select name="year">
-        <?php
-            $year =1960;
-            echo "<option value=''>-- 년도 --</option>";
-            while($year<=2015)
-            {
-              echo "<option value='$year'>$year</option>";
-              $year++;
-            }
-        ?>
-     </select>
-  
- 
-     <select name="month">
-        <?php
-            $month =1;
-            echo "<option value=''>-- 월 --</option>";
-            while($month<=12)
-            {
-              echo "<option value='$month'>$month</option>";
-              $month++;
-            }
-        ?>
-      </select>
-      <select name="day">
-        <?php
-            $day =1;
-            echo "<option value=''>-- 일 --</option>";
-            while($day<=31)
-            {
-              echo "<option value='$day'>$day</option>";
-              $day++;
-            }
-        ?>
-      </select>
-      </td>
-  </tr>
-
-  <tr>
-   <td> 휴대폰 </td>
-   <td>
-    <select name="firstNum">
-     <option> 010 </option>
-     <option> 011 </option>
-     <option> 016 </option>
-     <option> 018 </option>
-    </select> - 
-    <input type = "text" name="middleNum" size = "4" maxlength = "4" onkeypress='return checkNumber(event)'/> - <input type = "text" name="lastNum"size = "4" maxlength = "4" onkeypress='return checkNumber(event)'/>
-   </td>
-
-  </tr>
-  <input type = "submit" value = "가입하기"/>
-<input type = "button" onClick="window.location.reload()" value = "다시 입력"/>
-</FORM>
-</table>
-
-
-</body>
+				if(!document.register.FirstNum.value || !document.register.middleNum.value || !document.register.lastNum.value){
+					alert("휴대폰번호를 입력하세요.");
+					return false;
+				}
+			}
+		</script>
+	</head>
+	<body>
+		<h1>회원가입</h1>
+		<table boder = "" bgcolor = "#cdfdee" cellspacing = "1" >
+			<FORM name="register" method="post" action="./insertMember.php" onsubmit="return checkValue()">
+				<tr>
+					<td text-align="center">아이디 </td>
+					<td><input type = "text" name="id" maxlength = "10"/></td>
+					<td><input type = "button" value = "중복확인" onClick = "checkId()"/></td>
+				</tr>
+				<tr>
+					<td> 비밀번호 </td>
+					<td><input type = "password" name="pw" maxlength = "10"/></td>
+				</tr>
+				<tr>
+					<td> 비밀번호 확인 </td>
+					<td> <input type = "password" name="pwCheck" maxlength = "10"/> </td>
+				</tr>
+				<tr>
+					<td> 성명 </td>
+					<td> <input type = "text" name="name" maxlength = "10"/> </td>
+				</tr>
+				<tr>
+					<td> 생년월일 </td>
+					<td>
+						<select name="year">
+							<?php
+								$year =1960;
+								echo "<option value=''> 년도 </option>";
+								while($year<=2015){
+									echo "<option value='$year'>$year</option>";
+									$year++;
+								}
+							?>
+						</select>
+						<select name="month">
+							<?php
+								$month =1;
+								echo "<option value=''> 월 </option>";
+								while($month<=12){
+									echo "<option value='$month'>$month</option>";
+									$month++;
+								}
+							?>
+						</select>
+						<select name="day">
+							<?php
+								$day =1;
+								echo "<option value=''> 일 </option>";
+								while($day<=31){
+									echo "<option value='$day'>$day</option>";
+									$day++;
+								}
+							?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td> 휴대폰 </td>
+					<td>
+						<select name="firstNum">
+							<option> 010 </option>
+							<option> 011 </option>
+							<option> 016 </option>
+							<option> 018 </option>
+						</select> - 
+						<input type = "text" name="middleNum" size = "4" maxlength = "4" onkeypress='return checkNumber(event)'/> - <input type = "text" name="lastNum"size = "4" maxlength = "4" onkeypress='return checkNumber(event)'/>
+					</td>
+				</tr>
+			<input type = "submit" value = "가입하기"/>
+			<input type = "button" onClick="window.location.reload()" value = "다시 입력"/>
+			</FORM>
+		</table>
+	</body>
 </html>
