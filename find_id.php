7d14d7e789255afd468a3c00c5f411d6ac628670
ChoiXD <84668477+ChoiXD@users.noreply.github.com>
2021-05-27 22:30:53 +0900

    id/pw 찾기 php 수정 중

---
+++ find_id.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>ID 찾기</title>
+	<style>
+	</style>
+</head>
+<body>
+	<?php
+		include "stdlib.php";
+
+		$user_phone_number = 'phoneNumber';
+
+		search_member_id($user_phone_number);
+	?>
+</body>
+</html>
+
+
+
