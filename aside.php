970d930c82e214a9b2b94d3a6daa74016478daf0
Aierse <68111814+Aierse@users.noreply.github.com>
2021-05-27 18:27:49 +0900

    aside.php 생성

---
+++ aside.php
@@ -0,0 +1,23 @@
+<style>
+	aside {
+		position : absolute;
+		background-color : #fdfcf0;
+		width : 380.594px;
+		height : 655.891px;
+	}
+
+	aside > nav {
+		padding : 0 19.016px;
+	}
+</style>
+
+<aside>
+	<?php
+		include "loginform.php";
+	?>
+	<nav>
+		<?php
+			include "menu.php";
+		?>
+	</nav>
+</aside>
