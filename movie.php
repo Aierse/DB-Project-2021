9d0e0dfe2af1b9eeb19d3e28d63609a33f0a5395
Aierse <68111814+Aierse@users.noreply.github.com>
2021-05-28 22:25:17 +0900

    이미지 중앙정렬

--- movie.php
+++ movie.php
@@ -1,22 +1,20 @@
 <style>
 	#movie {
-		width : 100%;
-		height : 655.891px;
 		display : inline-block;
 		margin-top : 19.016px;
 	}
 
 	.prev, .next {
-		cursor: pointer;
-		position: absolute;
-		top: 50%;
-		width: auto;
-		padding: 16px;
-		margin-top: -22px;
-		color: #e71a0f;
-		font-weight: bold;
-		font-size: 36px;
-		transition-duration: 1s;
+		cursor : pointer;
+		position : absolute;
+		top : 50%;
+		width : auto;
+		padding : 16px;
+		margin-top : -22px;
+		color : #e71a0f;
+		font-weight : bold;
+		font-size : 36px;
+		transition-duration : 1s;
 	}
 
 	.next {
@@ -28,11 +26,31 @@
 	}
 
 	.prev:hover, .next:hover {
-		background-color: #e71a0f;
+		background-color : #e71a0f;
 		color : white;
 	}
+	.img_area {
+		height : 655.891px;
+		line-height : 655.891px;
+	}
+	.img_area img {
+		width : 300px;
+		vertical-align : middle;
+	}
 </style>
+<script>
+	var slideindex = 1;
+
+	function plusslides(n) {
+		slideindex += n;
+	}
+</script>
 <div id = "movie">
-	<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-	<a class="next" onclick="plusSlides(1)">&#10095;</a>
+	<form method = "POST" action = "index.php">
+		<a class="prev" onclick="plusslides(-1)">&#10094;</a>
+		<a class="next" onclick="plusslides(1)">&#10095;</a>
+	</form>
+	<div class = "img_area">
+		<img src="image/testimage.png" alt="이미지 불러오기에 실패했습니다.">
+	</div>
 </div>
