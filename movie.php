4b126182219853c24cd98a8a88e4dab6ae31b1de
Aierse <68111814+Aierse@users.noreply.github.com>
2021-05-30 19:54:38 +0900

    영화보기 CSS

--- movie.php
+++ movie.php
@@ -2,6 +2,7 @@
 	#movie {
 		display : inline-block;
 		margin-top : 19.016px;
+		margin-bottom : 19.016px;
 	}
 
 	.prev, .next {
@@ -29,13 +30,34 @@
 		background-color : #e71a0f;
 		color : white;
 	}
-	.img_area {
-		height : 655.891px;
-		line-height : 655.891px;
+
+	.name_area h1 {
+		padding-top : 19.016px;
+		padding-bottom : 19.016px;
+		padding-left : 16px;
+		letter-spacing : 16px;
 	}
+
 	.img_area img {
-		width : 300px;
+		width : 350px;
+		height : 416.814px;
+		padding : 25px;
 		vertical-align : middle;
+	}
+
+	.reserve {
+		outline : none;
+		background-color : #e71a0f;
+		color : white;
+		border : 1px solid gray;
+
+		font-weight: 700;
+		font-size : 24px;
+		width : 400px;
+		height : 65px;
+
+		letter-spacing : 30px;
+		padding-left : 30px;
 	}
 </style>
 <script>
@@ -50,7 +72,39 @@
 		<a class="prev" onclick="plusslides(-1)">&#10094;</a>
 		<a class="next" onclick="plusslides(1)">&#10095;</a>
 	</form>
-	<div class = "img_area">
-		<img src="image/testimage.png" alt="이미지 불러오기에 실패했습니다.">
-	</div>
+	<table border>
+		<tr class = "name_area">
+			<td><h1>테스트1</h1>
+			<td><h1>테스트2</h1>
+			<td><h1>테스트3</h1>
+		</tr>
+		<tr class = "img_area">
+			<td><img src="image/testimage.png" alt="이미지 불러오기에 실패했습니다."></td>
+			<td><img src="image/testimage.png" alt="이미지 불러오기에 실패했습니다."></td>
+			<td><img src="image/testimage.png" alt="이미지 불러오기에 실패했습니다."></td>
+		</tr>
+		<tr>
+			<?php
+				if (!isset($_SESSION['id'])) {
+					for ($i = 0; $i < 3; $i++) {
+						echo "<td>";
+						echo "<form>";
+						echo "<input type = 'submit' class = 'reserve' value = '비회원 예매'/>";
+						echo "</form>";
+						echo "</td>";
+					}
+				}
+
+				else {
+					for ($i = 0; $i < 3; $i++) {
+						echo "<td>";
+						echo "<form>";
+						echo "<input type = 'submit' class = 'reserve' value = '예매'/>";
+						echo "</form>";
+						echo "</td>";
+					}
+				}
+			?>
+		</tr>
+	</table>
 </div>
