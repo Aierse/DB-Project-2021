6d671899aac8188f6e49a099aa9f4f3e0fb8f122
ChoiXD <84668477+ChoiXD@users.noreply.github.com>
2021-06-10 11:59:56 +0900

    코드수정

--- reserve_page.php
+++ reserve_page.php
@@ -54,7 +54,7 @@
 	for ($i = 0; $i < count($screening); $i++) {
 		$month = (int)substr($screening[$i][2], 0, 2);
 		$day = (int)substr($screening[$i][2], 2, 2);
-		$hour = (int)substr($screening[$i][2], 4, 2)
+		$hour = (int)substr($screening[$i][2], 4, 2);
 		$minute = (int)substr($screening[$i][2], 6, 2);
 		$screening[$i][2] = $month."월 ".$day."일 ".$hour."시 ".($minute == 0 ? "" : $minute."분");
 		$room_id[$screening[$i][1]][count($room_id[$screening[$i][1]])] = array($screening[$i][0], $screening[$i][2]);
