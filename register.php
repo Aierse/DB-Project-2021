43913127949ca50359f456a0c0c5ad741dde1935
ChoiXD <84668477+ChoiXD@users.noreply.github.com>
2021-06-11 00:31:24 +0900

    코드 수정

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ko">
 	<head>
-		<<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 			<title>회원가입 페이지</title>
 		<style>
 			h, td, tr, input,button, textarea, select, FORM{
