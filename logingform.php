782a12335f0a1e1ae572b666e2cda0dc41a72327
Aierse <68111814+Aierse@users.noreply.github.com>
2021-06-12 20:36:25 +0900

    CSS 수정

--- logingform.php
+++ logingform.php
@@ -11,6 +11,10 @@
 		height : 212.5px;
 	}
 
+	#loging > div > p {
+		margin-bottom : 19.016px;
+	}
+
 	#loging > div > p > strong {
 		font-size : 30px;
 	}
@@ -29,8 +33,30 @@
 		cursor : pointer;
 	}
 
-	#account_menu > li {
+
+	#account_menu {
+		position : absolute;
+		width : 302.531px;
+		height : 103.5px;
+	}
+
+	#account_menu > ul {
+		margin-bottom : 20px;
 		list-style-type : none;
+	}
+
+	#account_menu > ul > li {
+		margin : 5px 0px;
+	}
+
+	#account_menu > ul > li > a {
+		padding : 10px 0px;
+		font-size : 20px;
+		color : #505050;
+	}
+
+	#account_menu > ul > li > a:visited {
+		color : #505050;
 	}
 </style>
 
@@ -40,18 +66,19 @@
 			<?php
 				include_once "stdlib.php";
 				$name = $_SESSION['name'];
-				echo "<strong>$name"."님</strong>"
+				echo "<strong>$name"."님</strong>";
 			?>
+			<h2>환영합니다</h2>
 		</p>
 		<nav id = "account_menu">
-			<li>
+			<ul>
 				<?php
 					if (is_member()) {
-						echo "<ul><a href = 'my_info.php'>내 정보</a></ul>";
+						echo "<li><a href = 'my_info.php'>내 정보</a></li>";
 					}
 				?>
-				<ul><a href = "check_reserve_page.php">예매 확인</a></ul>
-			</li>
+				<li><a href = "check_reserve_page.php">예매 확인</a></li>
+			</ul>
 		</nav>
 	</div>
 	<button class = "logout" onclick = "location.href='logout.php'">로그 아웃</button>
