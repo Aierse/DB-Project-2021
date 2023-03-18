722e1ad0d8276f29e5ebde38d84f3be33e75b527
Aierse <dbsgur2008@gmail.com>
2023-03-18 15:38:50 +0900

    feat: 푸드 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Seat from "./routes/Seat";
+import Food from "./routes/Food";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="menu">
+          <Route path="food" element={<Food />}></Route>
+        </Route>
       </Routes>
     </Router>
   );
